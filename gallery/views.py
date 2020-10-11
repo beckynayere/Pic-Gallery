c1cdae821523d758bd7592e8ebbfe272d2ef7d83
becky nayere <rebecca.nayere@gmail.com>
2020-10-11 17:19:46 +0300

    create a function for location,results in view

--- gallery/views.py
+++ gallery/views.py
@@ -7,3 +7,21 @@ def index(request):
     locations = Location.get_locations()
     print(locations)
     return render(request, 'gallery/index.html', {'images': images[::-1], 'locations': locations})
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pictures/location.html', {'location_images': images})
+
+
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'pictures/search_results.html', {"message": message})
+
