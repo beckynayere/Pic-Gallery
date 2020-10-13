5c85eb20bf43b18b07f71872e5bb75ff1907d9d2
becky nayere <rebecca.nayere@gmail.com>
2020-10-13 18:35:26 +0300

    add a special route for getting a specific image

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,9 +8,10 @@ app_name='galery'
 urlpatterns=[
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_results, name='search'),
+    url(r'^category/(\d+)', views.category, name='category'),
     url(r'^location/(?P<location>\w+)/', views.image_location, name='location')
 
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
